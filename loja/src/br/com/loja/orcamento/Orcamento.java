05fe66220e5bd88f2cc5c9b119ce647b421de60b
BrendonAlc <brendon_rodrigo@hotmail.com>
2023-03-27 22:58:40 -0300

    Aplicado getter valor e quantidadeDeItens e setter Situacao métodos para SituacaoOrcamento e aplicarDescontoExtra de acordo com valor gasto.

--- loja/src/br/com/loja/orcamento/Orcamento.java
+++ loja/src/br/com/loja/orcamento/Orcamento.java
@@ -2,14 +2,40 @@ package br.com.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.loja.orcamento.situacao.EmAnalise;
+import br.com.loja.orcamento.situacao.Finalizado;
+import br.com.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 
 	private BigDecimal valor;
 	private int quantidadeDeItens;
+	private SituacaoOrcamento situacao;
 	
 	public Orcamento(BigDecimal valor, int quantidadeDeItens) {
 		this.valor = valor;
 		this.quantidadeDeItens = quantidadeDeItens;
+		this.situacao =new EmAnalise();
+	}
+	
+	/**
+	 * Padrão State
+	 */
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
+	}
+	
+	public void aprovar() {
+		this.situacao.aprovar(this);
+	}
+
+	public void reprovar() {
+		this.situacao.reprovar(this);
+	}
+	
+	public void finalizar() {
+		this.situacao.finalizar(this);
 	}
 	
 	public BigDecimal getValor() {
@@ -20,4 +46,11 @@ public class Orcamento {
 		return quantidadeDeItens;
 	}
 	
+	public SituacaoOrcamento getSituacao() {
+		return situacao;
+	}
+	
+	public void setSituacao(SituacaoOrcamento finalizado) {
+		this.situacao = finalizado;
+	}
 }
